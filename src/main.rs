79a6196e9bb65badc6def06717679a5c10ba8f01
cuervo-blanco <jaios.228@gmail.com>
2024-07-30 18:34:25 -0400

    Simplified Threads, Message Receipt New, Print Thread Removed, Debug Print

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::net::TcpStream;
 use std::io::{Write, Read};
-use std::sync::{Arc, Mutex, Condvar};
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::time::Duration;
+
+const SEPARATOR: &str = "#";
+const TERMINATOR: &str = "\n";
 
 //---Definitions---//
 fn  clear_terminal() {
@@ -16,33 +21,16 @@ fn username_take()-> String {
     let instance_name = instance_name.replace("\n", "").replace(" ", "_");
     instance_name
 }
-fn  move_cursor(row: u32, col: u32) {
-    print!("\x1B[{};{}H", row, col);
-    std::io::stdout().flush().unwrap();
-}
+#[allow(dead_code)]
 fn demultiplex(text: String)-> Vec<String> {
     let mut spread = Vec::new();
-    if let Some(slash_index) = text.find("#") {
+    if let Some(slash_index) = text.find(SEPARATOR) {
         let username: &str = &text[..slash_index];
         spread.push(username.to_string());
         let message: &str = &text[slash_index + 1..];
         spread.push(message.to_string());
     }
     spread
-}
-fn write_at_position(row: u32, col: u32, text: String) {
-    move_cursor(row, col);
-    print!("{} ", text);
-    std::io::stdout().flush().unwrap();
-}
-type Position = (u32, u32);
-
-fn update_position(positions: Vec<Position>) -> Vec<Position>{
-    let mut updated_positions = Vec::new();
-    for (x, y) in positions {
-        updated_positions.push((x+1, y));
-    }
-    updated_positions
 }
 
 fn main () {
@@ -54,23 +42,76 @@ fn main () {
     // Add validation process? 
     #[allow(unused_variables)]
     let instance_name = username_take();
+    clear_terminal();
 
     println!("System preparing for take off...");
-    let first_column: Position = (1 as u32, 1 as u32);
-    let second_column: Position = (1 as u32, 20 as u32);
-    let positions: Vec<Position> = vec![first_column, second_column];
-    let shared_positions =  Arc::new((Mutex::new(positions.clone()), Condvar::new()));
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+    clear_terminal();
 
-    #[allow(unused_mut)]
-    let mut header = demultiplex("username#message".to_string()); 
-    // Equivalent to the header is the message, it's meant to carry the message 
-    // and pass it along the threads
-    #[allow(unused_mut)]
-    let mut message = Arc::new((Mutex::new(Vec::<String>::new()), Condvar::new()));
+    println!("");
+    // User table to store users discovered and their service information
+    let user_table: Arc<Mutex<HashMap<String, TcpStream>>> = Arc::new(Mutex::new(std::collections::HashMap::new()));
+    let user_table_clone = Arc::clone(&user_table);
+    println!("MAIN: Data Structures Initialized");
 
-    for (i, (row, col)) in positions.iter().enumerate() {
-        write_at_position(*row, *col, header[i].clone());
-    }    
+    // -------- Input Thread ------- //
+    std::thread::spawn ( move || {
+        println!("THREAD 1: Thread Initialized");
+        loop {
+            // Take user input
+            let reader = std::io::stdin();
+            let mut buffer: String = String::new();
+            reader.read_line(&mut buffer).unwrap();
+            let input = buffer.trim();
+            println!("THREAD 1: User Input: {}", input);
+
+            let user_table = user_table.lock().unwrap();
+            println!("THREAD 1: User Table Lock: {:?}", user_table);
+            // Send message to each socket upon user input Enter
+            // Write to the streams in those sockets
+            for (user, stream) in user_table.iter() {
+                // Clean up the name, get rid of .local
+                let mut username = String::new();
+
+                println!("THREAD 1: Found Username: {:?}", username);
+                for char in user.chars() {
+                    if char != '.' {
+                        username.push(char);
+                    } else {
+                        break;
+                    }
+
+                }
+                println!("THREAD 1: Display Username: {:?}", username);
+
+                let message = vec![username.to_string(), input.to_string(), TERMINATOR.to_string()];
+                println!("THREAD 1: Message Vector: {:?}", message);
+                let message = message.join(SEPARATOR);
+                println!("THREAD 1: Message to send: {:?}", message);
+                let encoded_message: Vec<u8> = bincode::serialize(&message).unwrap();
+                println!("THREAD 1: Encoded message: {:?}", encoded_message);
+
+                // Verify if this accessing of the operation is valid
+                println!("THREAD 1: Verifying stream: {:?}", stream);
+                match stream.try_clone() {
+                    Ok(mut stream) => {
+                        if let Err(e) = stream.write_all(&encoded_message) {
+                            eprintln!("THREAD 1: Failed to send message to {}: {}", user, e);
+                        }
+                    },
+                    Err(e) => {
+                        println!("THREAD 1: Failed to clone stream for {}: {}", username, e);
+                        continue;
+                    }
+                };
+                // Something must refresh the terminal every second or so clear out the display
+                // fetch the information from the data structure containing the streams and user names and print it
+                // Clear out the buffer 
+                println!("THREAD 1: RESTARTING LOOP #1");
+            }
+             println!("THREAD 1: RESTARTING MAIN LOOP");
+        }
+    });
 
     // A thread to receive the bytes coming from a tcp stream, 
     // convert the byte stream into a String (with a termination symbol
@@ -81,32 +122,10 @@ fn main () {
     // with t standig for text and e for end
     // When it packages the info it passes it to the print thread
     // that will update the current position and print the provided strings
-    
-    //------ The Printing Thread -----------//
-    // Start the print message thread and make it wait
-    let sp = Arc::clone(&shared_positions); 
-    let msg = Arc::clone(&message);
-    std::thread::spawn( move || {
-        // Taking the shared positions borrowed in order to make changes to it
-        let (p_lock, pcvar) = &*sp;
-        let mut positions = p_lock.lock().unwrap();
-        // Borrowing the message String to store the incoming message there
-        let (m_lock, mcvar) = &*msg;
-        let mut incoming_message = m_lock.lock().unwrap();
-
-        loop {
-            positions = pcvar.wait(positions).unwrap();
-            incoming_message = mcvar.wait(incoming_message).unwrap();
-            // Capture header and positions before doing this:
-            for (i, (row, col)) in positions.iter().enumerate() {
-                write_at_position(*row, *col, incoming_message[i].clone());
-            }    
-        }
-        
-    });
 
     //---- The TCP Thread -----//
 
+    println!("THREAD 2: Thread Initializing Parameters");
     // Get information from local host to start tcp stream
     let ip =  local_ip_address::local_ip().unwrap();
     let port: u16 = 18521;
@@ -115,73 +134,84 @@ fn main () {
     let listener = std::net::TcpListener::bind(socket_addr.clone())
         .expect("Failed to start listener");
 
-    println!("Starting TCP stream reader and text generator thread...");
-    let sp = Arc::clone(&shared_positions); 
-    let msg = Arc::clone(&message);
+    println!("THREAD 2: Starting TCP stream reader and text generator thread...");
     std::thread::spawn( move || {
-        for tcp_stream in listener.incoming() {
-            match tcp_stream {
-                Ok(stream) => {
-                    let mut stream = stream.try_clone().unwrap();
-                    println!("I arrive here fine: 1");
-                    let mut buffer = [0; 512];
-                    println!("I arrive here fine: 2");
-                    let bytes_read = stream.read(&mut buffer).unwrap();
-                    println!("I arrive here fine: 3");
+        println!("THREAD 2: Succesful Deployment of Thread.");
+        loop {
+            println!("THREAD 2: Entering Thread Loop.");
+            for tcp_stream in listener.incoming() {
+                println!("THREAD 2: Listening for incoming packets.");
+                match tcp_stream {
+                    Ok(stream) => {
+                        let mut stream = stream.try_clone().unwrap();
+                        println!("THREAD 2: Stream Cloned <{:?}>", stream);
+                        let mut buffer = [0; 512];
+                        println!("THREAD 2: Memory allocated to buffer: {:?}", buffer);
 
-                    let (p_lock, pcvar) = &*sp;
-                    println!("I arrive here fine: 4");
-                    let mut positions = p_lock.lock().unwrap();
-                    println!("I arrive here fine: 5");
-                    let (m_lock, mcvar) = &*msg;
-                    println!("I arrive here fine: 6");
-                    let mut incoming_message = match m_lock.lock() {
-                        Ok(msg) => msg,
-                        Err(e) => {
-                            println!("Error locking message: {}", e);
-                            std::process::exit(1);
-                        }
-                    };
+                        let bytes_read = stream.read(&mut buffer).unwrap();
+                        println!("THREAD 2: Incoming Bytes_Read: {:?}", &buffer[..bytes_read]);
+                        let incoming_message = &buffer[..bytes_read];
+                        println!("THREAD 2: Incoming Message: {:?}", incoming_message);
 
-                    println!("I arrive here fine: 7");
-                    let mut data = Vec::new();
+                        let mut data = Vec::new();
+                        println!("THREAD 2: Allocating Memory (DATA) for Incoming Message: data {:?}", data);
 
-                    println!("I arrive here fine: 8");
-                    data.extend_from_slice(&buffer[..bytes_read]);
-                    println!("I arrive here fine: 9");
-                    if let Some(pos) = data.iter().position(|&b| b == b'\n') {
-                        println!("I arrive here fine: 10");
-                        let text = data.split_off(pos + 1);
-                        println!("I arrive here fine: 11");
+                        data.extend_from_slice(&buffer[..bytes_read]);
+                        println!("THREAD 2: Saving buffer: data {:?}", data);
+                        let filtered_data: Vec<u8> = data
+                            .into_iter()
+                            .filter(|&b| b.is_ascii_graphic() || b.is_ascii_whitespace())
+                            .collect();
+                        let mut msg: String = match String::from_utf8(filtered_data) {
+                            Ok(s) => {
+                                println!("THREAD 2: Converted string: {}", s);
+                                s
+                            },
+                            Err(e) => { 
+                                eprintln!("THREAD 2: Failed to convert bytes to string: {}", e);
+                                std::process::exit(1);
+                                
+                            }
+                        };
+                        
+                            println!("THREAD 2: Message: {:?}", msg);
+                            fn find_position(string: &String, end_mark: char) -> usize {
+                                if let Some(pos) = string.chars().position(|b| b == end_mark){
+                                    return pos
+                                } else {
+                                    println!(" THREAD 2: Failed to get position");
+                                    std::process::exit(2);
+                                }
+                            }
+                            let pos = find_position(&msg, '\n');
+                            println!("THREAD 2: Finding position of message_end: {:?}", pos);
+                            let text = msg.split_off(pos + 1);
+                            println!("THREAD 2: Splitting message: {}", text);
 
-                        if let Ok(string) = std::str::from_utf8(&text) {
                             // Update the positions 
                             // Send the header to the print thread
-                            let string: String = string.to_string();
+                            let string: String = msg.to_string();
+                            println!("THREAD 2: Converting message to string: {}", string);
                             let mut spread = Vec::new();
+                            println!("THREAD 2: Allocating Memory for received message: {:?}", spread);
 
-                            if let Some(slash_index) = string.find("/#t/") {
+                            if let Some(slash_index) = string.find(SEPARATOR) {
                                 let username: &str = &string[..slash_index];
                                 spread.push(username.to_string());
-                                let message: &str = &string[slash_index + 4..];
+                                let message: &str = &string[slash_index + 1..string.len() - 2];
                                 spread.push(message.to_string());
                             }
-
-                            *incoming_message = spread;
-                            *positions = update_position(positions.to_vec());
-
-                            pcvar.notify_one();
-                            mcvar.notify_one();
+                            println!("THREAD 2: Cleaning up message: {:?}", spread);
+                            println!("{}", spread.join(" "));
                             
-                        }
                         
                     }
-                    
+                    Err(e) => println!("Error getting stream: {}", e),
                 }
-                Err(e) => println!("Error getting stream: {}", e),
             }
+            println!("THREAD 2: Restarting first loop");
         }
-    }).join().unwrap();
+    });
 
     // ----------- mDNS Service Thread ----------//
     
@@ -217,98 +247,48 @@ fn main () {
     let receiver = mdns.browse(service_type).expect("Failed to browse");
     println!("Browsing for services");
 
-    // User table to store users discovered and their service information
-    let user_table = Arc::new(Mutex::new(std::collections::HashMap::new()));
-    let user_table_clone = Arc::clone(&user_table);
 
     println!("Starting mDNS service thread...");
 
     // Listen for Services, Respond & Store
     std::thread::spawn(move || {
-        while let Ok(event) = receiver.recv() {
-            match event {
-                mdns_sd::ServiceEvent::ServiceResolved(info) => {
-                    println!("Service resolved: {:?}", info);
-                    // Send request to create tcp connection
-                    let addresses = info.get_addresses_v4();
-                    for address in addresses {
-                        let mut user_table_clone = user_table_clone.lock().unwrap();
-                        let user_socket = format!("{}:{}", address, info.get_port());
-                        // --------- Tcp Connection ---------//
-                        match std::net::TcpStream::connect(&user_socket){
-                            Ok(stream) => {
-                                user_table_clone.insert(info.get_fullname().to_string(), stream);
-                                let mut username = String::new();
-                                for char in info.get_fullname().chars() {
-                                    if char != '.' {
-                                        username.push(char);
-                                    } else {
-                                        break;
+        loop {
+            while let Ok(event) = receiver.recv() {
+                match event {
+                    mdns_sd::ServiceEvent::ServiceResolved(info) => {
+                        println!("Service resolved: {:?}", info);
+                        // Send request to create tcp connection
+                        let addresses = info.get_addresses_v4();
+                        for address in addresses {
+                            let mut user_table_clone = user_table_clone.lock().unwrap();
+                            let user_socket = format!("{}:{}", address, info.get_port());
+                            // --------- Tcp Connection ---------//
+                            match std::net::TcpStream::connect(&user_socket){
+                                Ok(stream) => {
+                                    user_table_clone.insert(info.get_fullname().to_string(), stream);
+                                    let mut username = String::new();
+                                    for char in info.get_fullname().chars() {
+                                        if char != '.' {
+                                            username.push(char);
+                                        } else {
+                                            break;
+                                        }
                                     }
-                                }
-                                println!("{} just connected", username);
-                            },
-                            Err(e) => println!("Failed to connect to user {}: {}", user_socket, e),
+                                    println!("{} just connected", username);
+                                },
+                                Err(e) => println!("Failed to connect to user {}: {}", user_socket, e),
+                            }
                         }
-                    }
-                },
-                _ => {
+                    },
+                    _ => {
 
+                    }
                 }
             }
         }
-    });
+    }).join().unwrap();
 
     // Optional: Show Services Discovered
-
-    // -------- Input Thread ------- //
-    loop {
-        // Take user input
-        let reader = std::io::stdin();
-        let mut buffer: String = String::new();
-        reader.read_line(&mut buffer).unwrap();
-        let input = buffer.trim();
-        println!("Input: {}", input);
-
-        let user_table = user_table.lock().unwrap();
-        // Send message to each socket upon user input Enter
-        // Write to the streams in those sockets
-        for (user, stream) in user_table.iter() {
-            // Clean up the name, get rid of .local
-            let mut username = String::new();
-            for char in user.chars() {
-                if char != '.' {
-                    username.push(char);
-                } else {
-                    break;
-                }
-
-            }
-
-            let message = vec![username.to_string(), input.to_string()];
-            let message = message.join("#");
-            println!("Message to send: {}", message);
-            let encoded_message: Vec<u8> = bincode::serialize(&message).unwrap();
-            println!("Encoded message: {:?}", encoded_message);
-
-            // Verify if this accessing of the operation is valid
-            match stream.try_clone() {
-                Ok(mut stream) => {
-                    if let Err(e) = stream.write_all(&encoded_message) {
-                        eprintln!("Failed to send message to {}: {}", user, e);
-                    }
-                },
-                Err(e) => {
-                    println!("Failed to clone stream for {}: {}", username, e);
-                    continue;
-                }
-            };
-            // Something must refresh the terminal every second or so clear out the display
-            // fetch the information from the data structure containing the streams and user names and print it
-            // Clear out the buffer 
-        }
-    }
-
 
 }
 
